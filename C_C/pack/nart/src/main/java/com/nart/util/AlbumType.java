12b140e6555ef7086ac6ae91ce6f16c5216f6202
zirui <ziruiqiao@cmail.carleton.ca>
2023-02-19 16:27:22 -0500

    change tag

--- C_C/pack/nart/src/main/java/com/nart/util/AlbumType.java
+++ C_C/pack/nart/src/main/java/com/nart/util/AlbumType.java
@@ -7,8 +7,8 @@ package com.nart.util;
  * @className: AlbumType
  * @Description: TODO
  * @version: v1.8.0
- * @author: ZIRUI QIAO
- * @date: 2023-01-31 2:43 p.m.
+ * @Author: ZIRUI QIAO
+ * @Date: 2023-01-31 2:43 p.m.
  */
 public enum AlbumType {
     USER_AVATAR, GROUP_AVATAR, STATUS_PICS, CHAT_PICS
